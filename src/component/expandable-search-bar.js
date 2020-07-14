a9f23018ec57675396ddce0a35577643d87e4fc1
Rafael Aquino <rafael.aquinoquijano@gmail.com>
2020-07-14 06:35:15 -0600

    initial commit

---
+++ src/component/expandable-search-bar.js
@@ -0,0 +1,6 @@
+document.querySelector('.search__submit').addEventListener('click', (e)=>{
+    console.log(e);
+});
+document.querySelector('.search__input').addEventListener('keyup', (e) => {
+    console.log(e.key);
+});
